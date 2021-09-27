d39b480cd10f8e163b710836e086b488b7937136
Paulius Grigaliunas <pg5989@gmail.com>
2021-09-27 23:22:51 +0300

    node modules

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const http = require('http');
+var http = require('http');
+var fs = require('fs');
 
-const hostname = '0.0.0.0';
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World, Paulius here\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+http.createServer(function(req, res){
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(8000);
