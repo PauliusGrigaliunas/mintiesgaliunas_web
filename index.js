4e24c70c44f332c2bd5b2acad2196c09c96b3172
Paulius Grigaliunas <pg5989@gmail.com>
2021-09-27 22:46:21 +0300

    updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end(<div>'Hello World\n'<div>Paulius here!</div></div>);
 });
 
 server.listen(port, hostname, () => {
